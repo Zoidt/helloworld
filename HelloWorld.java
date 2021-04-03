985a4c7dbad1b9e600898bff37a7fc45f7d5eac0
ztabana <zaidtabana@gmail.com>
2021-04-03 00:15:17 -0400

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Zaid")
+        System.out.println("Hello, Zaid");
+        System.out.println("Hello, bla bla bla");
+
     }
 
 }
